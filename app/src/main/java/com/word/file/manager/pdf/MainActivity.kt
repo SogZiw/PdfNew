6df9e0e6084e146a6323b618040d3faab963bbbb
SogZiw <sogziw@foxmail.com>
2026-04-24 18:04:12 +0800

    feat: update main page assets and resources

--- app/src/main/java/com/word/file/manager/pdf/MainActivity.kt
+++ app/src/main/java/com/word/file/manager/pdf/MainActivity.kt
@@ -6,11 +6,9 @@ import com.word.file.manager.pdf.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-    override val applyRootInsets: Boolean = true
-
     override fun setViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
 
     override fun initView() {
-        // init view here
+
     }
 }
