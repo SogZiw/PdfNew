025766c32e7115e40f1590ffe963745aa52fd07b
SogZiw <sogziw@foxmail.com>
2026-05-08 16:12:41 +0800

    delete ad

--- app/src/main/java/com/word/file/manager/pdf/modules/guide/GuideReasonActivity.kt
+++ app/src/main/java/com/word/file/manager/pdf/modules/guide/GuideReasonActivity.kt
@@ -4,16 +4,14 @@ import android.content.Intent
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.AppCompatImageView
-import com.word.file.manager.pdf.AD_POS_ID
-import com.word.file.manager.pdf.APP_AD_CHANCE
 import com.word.file.manager.pdf.R
-import com.word.file.manager.pdf.hasGoSettings
 import com.word.file.manager.pdf.base.BaseActivity
 import com.word.file.manager.pdf.base.helper.EventCenter
 import com.word.file.manager.pdf.base.helper.UserBlockHelper
 import com.word.file.manager.pdf.base.helper.ad.center.AdCenter
 import com.word.file.manager.pdf.base.helper.ad.model.NativeAdStyle
 import com.word.file.manager.pdf.databinding.ActivityGuideReasonBinding
+import com.word.file.manager.pdf.hasGoSettings
 import com.word.file.manager.pdf.modules.MainActivity
 import com.word.file.manager.pdf.modules.permissions.createAppDetailsSettingsIntent
 
@@ -21,13 +19,7 @@ class GuideReasonActivity : BaseActivity<ActivityGuideReasonBinding>() {
 
     private val settingsLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         hasGoSettings = false
-        EventCenter.logEvent(APP_AD_CHANCE, mapOf(AD_POS_ID to BACK_AD_EVENT))
-        AdCenter.backInterstitial.showFullScreen(
-            activity = activity,
-            eventName = BACK_AD_EVENT,
-            allowed = { UserBlockHelper.canShowExtra() },
-            closed = { goMainPage() },
-        )
+        goMainPage()
         EventCenter.logEvent("uninstall_back_to_app")
     }
 
@@ -94,9 +86,5 @@ class GuideReasonActivity : BaseActivity<ActivityGuideReasonBinding>() {
     override fun onDestroy() {
         hasGoSettings = false
         super.onDestroy()
-    }
-
-    private companion object {
-        const val BACK_AD_EVENT = "ad_back_int"
     }
 }
